8c4cf1bd6fea856c9862e9df38331bee406aea59
Max Ghenis <mghenis@gmail.com>
2025-11-01 20:45:28 +0000

    Fix welcome-vision config: remove unused imports

--- slides/slideshows/welcome-vision/config.ts
+++ slides/slideshows/welcome-vision/config.ts
@@ -1,15 +1,9 @@
 import { SlideshowConfig } from '@/lib/types';
 import SectionTitle from './slides/SectionTitle';
-import CoverSlide from './slides/CoverSlide';
 import JourneyBeginning from './slides/JourneyBeginning';
 import RecognisingOpportunity from './slides/RecognisingOpportunity';
 import EarlyGrowth from './slides/EarlyGrowth';
 import IntroducingAnvar from './slides/IntroducingAnvar';
-import AnvarIntro from './slides/AnvarIntro';
-import PostNufFieldTransformation from './slides/PostNufFieldTransformation';
-import TodaysAgenda from './slides/TodaysAgenda';
-import TodaysUnveiling from './slides/TodaysUnveiling';
-import EndSlide from './slides/EndSlide';
 
 export const welcomeVisionConfig: SlideshowConfig = {
   id: 'welcome-vision',
